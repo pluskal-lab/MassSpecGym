9c01c97a393ceacc6899c7387905ac7bfbee8131
roman-bushuiev <ro3721@gmail.com>
2024-05-17 10:28:01 +0200

    Simplify spectrum preprocessor

--- massspecgym/preprocessors.py
+++ massspecgym/preprocessors.py
@@ -13,14 +13,6 @@ class SpecPreprocessor:
         1. Apply a series of matchms filters to the input spectrum (method `matchms_transforms`).
         2. Convert the matchms spectrum to a numpy array (method `matchms_to_numpy`).
     """
-    # def from_matchms(self, spec: matchms.Spectrum) -> np.ndarray:
-    #     """
-    #     Apply a series of matchms filters to the input spectrum and convert it to a numpy array.
-    #     """
-    #     raise NotImplementedError('This method must be implemented in a SpecPreprocessor subclass.')
-
-    # def __call__(self, spec) -> Any:
-    #     return self.matchms_to_numpy(self.matchms_transforms(spec))
 
     def matchms_transforms(self, spec: matchms.Spectrum) -> matchms.Spectrum:
         """
@@ -35,50 +27,46 @@ class SpecPreprocessor:
         raise NotImplementedError('This method must be implemented in a SpecPreprocessor subclass.')
 
     def __call__(self, spec: matchms.Spectrum) -> np.ndarray:
+        """
+        Compose the matchms filters and the numpy conversion.
+        """
         return self.matchms_to_numpy(self.matchms_transforms(spec))
 
 
-class SpecFilter(SpecPreprocessor):
-    def __init__(
-        self,
-        max_peaks_n: int = 60,
+def default_matchms_transforms(
+        spec: matchms.Spectrum,
+        n_max_peaks: int = 60,
         mz_from: float = 10,
         mz_to: float = 1000
-    ) -> None:
-        super().__init__()
-        self.max_peaks_n = max_peaks_n
-        self.mz_from = mz_from
-        self.mz_to = mz_to
-
-    def matchms_transforms(
-        self,
-        spec: matchms.Spectrum, 
     ) -> matchms.Spectrum:
-        spec = ms_filters.normalize_intensities(spec)
-        spec = ms_filters.reduce_to_number_of_peaks(spec, n_max=self.max_peaks_n)
-        spec = ms_filters.select_by_mz(spec, mz_from=self.mz_from, mz_to=self.mz_to)
-        return spec
+    spec = ms_filters.normalize_intensities(spec)
+    spec = ms_filters.reduce_to_number_of_peaks(spec, n_max=n_max_peaks)
+    spec = ms_filters.select_by_mz(spec, mz_from=mz_from, mz_to=mz_to)
+    return spec
 
-class SpecTokenizer(SpecFilter):
+
+class SpecTokenizer(SpecPreprocessor):
     def __init__(
         self,
-        pad_to_n_peaks: Optional[int] = 60,
-        **kwargs
+        n_peaks: Optional[int] = 60,
     ) -> None:
-        super().__init__(**kwargs)
-        self.pad_to_n_peaks = pad_to_n_peaks
+        self.n_peaks = n_peaks
+
+    def matchms_transforms(self, spec: matchms.Spectrum) -> matchms.Spectrum:
+        return default_matchms_transforms(spec, n_max_peaks=self.n_peaks)
 
     def matchms_to_numpy(self, spec: matchms.Spectrum) -> np.ndarray:
         """
-        Stack arrays of mz and intensities into a matrix of shape (num_peaks, 2)
+        Stack arrays of mz and intensities into a matrix of shape (num_peaks, 2).
+        If the number of peaks is less than `n_peaks`, pad the matrix with zeros.
         """
         spec = np.vstack([spec.peaks.mz, spec.peaks.intensities]).T
-        if self.pad_to_n_peaks is not None:
-            spec = utils.pad_spectrum(spec, self.pad_to_n_peaks)
+        if self.n_peaks is not None:
+            spec = utils.pad_spectrum(spec, self.n_peaks)
         return spec
 
 
-class SpecBinner(SpecFilter):
+class SpecBinner(SpecPreprocessor):
     # TODO
     pass
 
