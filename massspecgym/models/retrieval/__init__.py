0afbf420805469e8bc3b094beded7f0e1ad420eb
Anton Bushuiev <anton.bushuiev@gmail.com>
2024-05-31 15:11:18 +0200

    Implement FingerprintFFN basline for retrieval

--- massspecgym/models/retrieval/__init__.py
+++ massspecgym/models/retrieval/__init__.py
@@ -1,5 +1,11 @@
 from .base import RetrievalMassSpecGymModel
 from .random import RandomRetrieval
 from .deepsets import DeepSetsRetrieval
+from .fingerprint_ffn import FingerprintFFNRetrieval
 
-__all__ = ["RetrievalMassSpecGymModel", "RandomRetrieval", "DeepSetsRetrieval"]
+__all__ = [
+    "RetrievalMassSpecGymModel",
+    "RandomRetrieval",
+    "DeepSetsRetrieval",
+    "FingerprintFFNRetrieval"
+]
