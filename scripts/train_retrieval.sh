19e0aaf6421d59386c4c28c8cde43286ae763e69
Anton Bushuiev <anton.bushuiev@gmail.com>
2024-06-03 18:38:38 +0200

    Add checkpointing

--- scripts/train_retrieval.sh
+++ scripts/train_retrieval.sh
@@ -13,4 +13,6 @@ export SLURM_GPUS_PER_NODE=8
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 srun --export=ALL --preserve-env python3 train_retrieval.py \
-    --run_name=debug
+    --job_key="${job_key}" \
+    --run_name=debug_0.5_v3 \
+    --val_check_interval=0.5
