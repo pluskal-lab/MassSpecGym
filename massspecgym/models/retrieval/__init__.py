22588a8e4151773cc1cd13f90929bf652e6526a7
Anton Bushuiev <anton.bushuiev@gmail.com>
2024-06-05 21:54:35 +0200

    Implement FromDictRetrieval baseline

--- massspecgym/models/retrieval/__init__.py
+++ massspecgym/models/retrieval/__init__.py
@@ -2,10 +2,12 @@ from .base import RetrievalMassSpecGymModel
 from .random import RandomRetrieval
 from .deepsets import DeepSetsRetrieval
 from .fingerprint_ffn import FingerprintFFNRetrieval
+from .from_dict import FromDictRetrieval
 
 __all__ = [
     "RetrievalMassSpecGymModel",
     "RandomRetrieval",
     "DeepSetsRetrieval",
-    "FingerprintFFNRetrieval"
+    "FingerprintFFNRetrieval",
+    "FromDictRetrieval"
 ]
