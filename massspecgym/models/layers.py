f7d2ac71783a61bc102884f720dbfe903326de95
roman-bushuiev <ro3721@gmail.com>
2024-08-10 21:43:43 +0200

    Black refactor

--- massspecgym/models/layers.py
+++ massspecgym/models/layers.py
@@ -4,31 +4,41 @@ import torch
 import torch.nn as nn
 from math import ceil
 
+
 class FourierFeatures(nn.Module):
     """
     A module for generating Fourier features for input data. This module maps input data
-    to a higher-dimensional space using sinusoidal functions, enhancing the representation 
+    to a higher-dimensional space using sinusoidal functions, enhancing the representation
     capabilities for various tasks.
 
     Args:
-        strategy (str): Strategy for generating frequency components. Available options are 
+        strategy (str): Strategy for generating frequency components. Available options are
             'random', 'voronov_et_al', and 'dreams'. Each option corresponds to a certain paper:
             - 'random': https://doi.org/10.48550/arXiv.2006.10739.
             - 'voronov_et_al': https://doi.org/10.48550/arXiv.2207.02980.
             - 'dreams': https://doi.org/10.26434/chemrxiv-2023-kss3r-v2.
         x_min (float, optional): The minimum value for generating frequencies. Defaults to 1e-4.
         x_max (float, optional): The maximum value for generating frequencies. Defaults to 1000.
-        trainable (bool, optional): If True, the frequencies are treated as trainable parameters. 
+        trainable (bool, optional): If True, the frequencies are treated as trainable parameters.
             Defaults to False.
-        funcs (str, optional): Specifies the trigonometric functions to use. Options are 'both', 
+        funcs (str, optional): Specifies the trigonometric functions to use. Options are 'both',
             'sin', and 'cos'. Defaults to 'both'.
-        sigma (float, optional): Standard deviation used for random frequency initialization 
+        sigma (float, optional): Standard deviation used for random frequency initialization
             when strategy is 'random'. Defaults to 10.
         num_freqs (int, optional): Number of frequency components to generate. Defaults to 512.
     """
 
-    def __init__(self, strategy, x_min=1e-4, x_max=1000, trainable=False, funcs='both', sigma=10, num_freqs=512):
-        assert funcs in {'both', 'sin', 'cos'}, "funcs must be 'both', 'sin', or 'cos'"
+    def __init__(
+        self,
+        strategy,
+        x_min=1e-4,
+        x_max=1000,
+        trainable=False,
+        funcs="both",
+        sigma=10,
+        num_freqs=512,
+    ):
+        assert funcs in {"both", "sin", "cos"}, "funcs must be 'both', 'sin', or 'cos'"
         assert 0 < x_min < 1, "x_min must be a positive fraction"
 
         super().__init__()
@@ -37,23 +47,26 @@ class FourierFeatures(nn.Module):
         self.trainable = trainable
         self.num_freqs = num_freqs
 
-        if strategy == 'random':
+        if strategy == "random":
             self.b = torch.randn(num_freqs) * sigma
-        elif self.strategy == 'voronov_et_al':
+        elif self.strategy == "voronov_et_al":
             self.b = torch.tensor(
-                [1 / (x_min * (x_max / x_min) ** (2 * i / (num_freqs - 2))) for i in range(1, num_freqs)],
+                [
+                    1 / (x_min * (x_max / x_min) ** (2 * i / (num_freqs - 2)))
+                    for i in range(1, num_freqs)
+                ],
             )
-        elif self.strategy == 'dreams':
+        elif self.strategy == "dreams":
             self.b = torch.tensor(
-                [1 / (x_min * i) for i in range(2, ceil(1 / x_min), 2)] +
-                [1 / (1 * i) for i in range(2, ceil(x_max), 1)],
+                [1 / (x_min * i) for i in range(2, ceil(1 / x_min), 2)]
+                + [1 / (1 * i) for i in range(2, ceil(x_max), 1)],
             )
         else:
             raise ValueError(f"Unknown strategy: {strategy}")
-        
+
         self.b = self.b.unsqueeze(0)
         self.b = nn.Parameter(self.b, requires_grad=self.trainable)
-        self.register_parameter('Fourier frequencies', self.b)
+        self.register_parameter("Fourier frequencies", self.b)
 
     @property
     def num_features(self):
@@ -64,7 +77,7 @@ class FourierFeatures(nn.Module):
         Returns:
             int: The number of features.
         """
-        return self.b.shape[1] if self.funcs != 'both' else 2 * self.b.shape[1]
+        return self.b.shape[1] if self.funcs != "both" else 2 * self.b.shape[1]
 
     def forward(self, x):
         """
@@ -78,11 +91,11 @@ class FourierFeatures(nn.Module):
         """
         x = 2 * torch.pi * x @ self.b
 
-        if self.funcs == 'both':
+        if self.funcs == "both":
             x = torch.cat((torch.cos(x), torch.sin(x)), dim=-1)
-        elif self.funcs == 'cos':
+        elif self.funcs == "cos":
             x = torch.cos(x)
-        elif self.funcs == 'sin':
+        elif self.funcs == "sin":
             x = torch.sin(x)
-        
+
         return x
