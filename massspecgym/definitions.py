4e82803b9929373398a6095e24ca785c7bb12001
roman-bushuiev <ro3721@gmail.com>
2024-08-14 13:19:23 +0200

    Add special tokens to definitions

--- massspecgym/definitions.py
+++ massspecgym/definitions.py
@@ -6,3 +6,9 @@ MASSSPECGYM_ROOT_DIR = pathlib.Path(__file__).parent.absolute()
 MASSSPECGYM_REPO_DIR = MASSSPECGYM_ROOT_DIR.parent
 MASSSPECGYM_DATA_DIR = MASSSPECGYM_REPO_DIR / 'data'
 MASSSPECGYM_TEST_RESULTS_DIR = MASSSPECGYM_DATA_DIR / 'test_results'
+
+# Special tokens
+PAD_TOKEN = "<pad>"
+SOS_TOKEN = "<s>"
+EOS_TOKEN = "</s>"
+UNK_TOKEN = "<unk>"
